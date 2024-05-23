3f87ca95bc13b464a80b55961aecbd393685a28a
fksw <fukatomo9@gmail.com>
2024-05-23 14:35:22 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-    //return view('start');
+    //return view('welcome');
+    return view('start');
 });
 
 Route::get('/dashboard', function () {
