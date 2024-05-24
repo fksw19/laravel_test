7395e28fc0282037151875121d0be667f2230d97
fksw19 <fukatomo9@gamil.com>
2024-05-24 10:15:41 +0900

    コメントtest

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,35 @@
 <?php
 
+// ProfileController クラスを使用します。
 use App\Http\Controllers\ProfileController;
+
+// Laravel のルーティング機能を使用します。
 use Illuminate\Support\Facades\Route;
 
+// ホームページの表示用ルート
 Route::get('/', function () {
-    //return view('welcome');
-    return view('start');
+    // welcome ビューを返します。
+    return view('welcome');
+    //return view('start');
 });
 
+// ダッシュボードの表示用ルート。認証とメール認証が必要です。
 Route::get('/dashboard', function () {
+    // dashboard ビューを返します。
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// 認証が必要なグループ内のルート
 Route::middleware('auth')->group(function () {
+    // プロフィールの編集用ルート
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    
+    // プロフィールの更新用ルート
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    
+    // プロフィールの削除用ルート
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// 認証関連のルートを別ファイルから読み込みます。
 require __DIR__.'/auth.php';
